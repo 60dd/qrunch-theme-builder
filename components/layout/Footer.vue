8da479468892a04102484715152eb2a6c6d7821c
rokuzeudon <g-awaya@6q.design>
2020-02-05 19:37:28 +0900

    html: 文字化けを修正

--- components/layout/Footer.vue
+++ components/layout/Footer.vue
@@ -4,7 +4,7 @@
         .main-footer__inner
             span.main-footer__blog-title
                 a.main-footer__blog-title-link(href="/") Blog Title
-            span.main-footer__blog-copyright Â©2018-2019
+            span.main-footer__blog-copyright © 2018-2019
                 a.main-footer__blog-copyright-user-link(href="#" target="_blank") @User Screen Name
                 | , Powered by 
                 a.main-footer__blog-copyright-qrunch-link(href="https://qrunch.net" target="_blank") qrunch
